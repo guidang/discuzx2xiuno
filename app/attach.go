0539782c1645f91579f5eb2ec2a679f78e4f14ad
skiy <dev@skiy.net>
2016-06-14 01:15:35 +0800

    添加附件初始化

---
+++ app/attach.go
@@ -0,0 +1,6 @@
+package app
+
+/**
+<p> <a href="upload/attach/20160607/1__E5_A4_87_E6_A1_88_E5_9B_BE_E6_A0_87_1465307326.png" target="_blank"><i class="icon filetype small image"></i>备案图标.png</a> &nbsp; <br></p>
+<p><br></p><p><img src="upload/attach/20160607/1__E8_8B_B9_E6_9E_9Capp_E5_86_85_E8_B4_AD_E6_94_AF_E4_BB_98_E6_B5_81_E7_A8_8B_1465307339.jpg" width="720" height="1112"><br></p>
+ */
